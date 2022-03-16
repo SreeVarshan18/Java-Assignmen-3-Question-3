f42d7f5ca14bf750eddda853bd0b2f7dae0801c1
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-16 15:28:49 +0530

    main

---
+++ src/com/harman/question3/Mainfile.java
@@ -0,0 +1,16 @@
+package com.harman.question3;
+
+import java.util.Scanner;
+
+public class Mainfile {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Integer a, b;
+        System.out.println("Enter 1st Number Greater than 10000");
+        a = input.nextInt();
+        System.out.println("Enter 2nd Number Greater than 10000");
+        b = input.nextInt();
+    }
+}
