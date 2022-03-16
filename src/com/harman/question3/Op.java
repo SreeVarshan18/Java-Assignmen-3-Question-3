dfa0720facf36dfc23efd1b8a2cbd1ad2023d424
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-16 15:36:48 +0530

    operations done

---
+++ src/com/harman/question3/Op.java
@@ -0,0 +1,17 @@
+package com.harman.question3;
+
+public class Op {
+
+    public int sum(int x, int y){
+        return x+y;
+    }
+    public int diff(int x, int y){
+        return x-y;
+    }
+    public int prod(int x, int y){
+        return x*y;
+    }
+    public int avg(int x, int y){
+        return (x+y)/2;
+    }
+}
